31cb62c60c606fc1b27f4388a3b6c099232de3dc
SurajGajula <ssamgajula@gmail.com>
2024-06-08 13:38:16 -0700

    Level Reset

--- PauseButton.cs
+++ PauseButton.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class PauseButton : MonoBehaviour{
-   private bool IsPaused = false;
-   public GameObject Tint;
+    private bool IsPaused = false;
+    public GameObject Tint;
+    public GameObject Restart;
     public void Pause(){
         IsPaused = !IsPaused;
         if (IsPaused){
+            Restart.SetActive(true);
             Tint.SetActive(true);
             Time.timeScale = 0f; 
         }
         else{
+            Restart.SetActive(false);
             Tint.SetActive(false);
             Time.timeScale = 1f;
         }
---
+++ ResetButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ResetButton : MonoBehaviour{
+     public void Reset(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f; 
+    }
+}
