476a4d46167d25d4e08f14ec9c0bc285b5b7ce7c
SurajGajula <ssamgajula@gmail.com>
2024-06-08 14:23:17 -0700

    Level Select

---
+++ LevelSelect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelSelect : MonoBehaviour{
+    public void SelectLevel(int Level){
+        if(Level == 1){
+            SceneManager.LoadScene("SampleLevel");
+        }
+    }
+}
