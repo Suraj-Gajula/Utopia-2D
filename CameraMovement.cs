e5f026845775c0e80058edb8021119d0fba93ec7
SurajGajula <ssamgajula@gmail.com>
2024-06-14 14:03:02 -0700

    Player Animation

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 public class CameraMovement : MonoBehaviour{
    public Transform Target;
+   public Transform Background;
    void Update(){
       transform.position = Target.position + new Vector3(0, 0, -10);
+      Background.position = Target.position + new Vector3(0, 0, 1);;
    }
 }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -10,8 +10,10 @@ public class PlayerMovement : MonoBehaviour{
     public bool IsDashing;
     private Vector2 StartPos;
     private int Direction;
+    public Animator PlayerAnimator;
     void Start(){
         PlayerBody = GetComponent<Rigidbody2D>();
+        PlayerAnimator = GetComponent<Animator>();
     }
     void Update(){
         if (Input.touchCount > 0){
@@ -29,8 +31,14 @@ public class PlayerMovement : MonoBehaviour{
         else{
             Direction = 0;
         }
-        if (transform.position.y < -10){
+        if(transform.position.y < -10){
             Respawn();
+        }
+        if(PlayerBody.velocity.x != 0){
+            PlayerAnimator.SetBool("IsMoving", true);
+        }
+        else{
+            PlayerAnimator.SetBool("IsMoving", false);
         }
     }
     void ButtonHandling(int TouchIndex){
@@ -68,12 +76,14 @@ public class PlayerMovement : MonoBehaviour{
     {
         HasDashed = true;
         IsDashing = true;
+        PlayerAnimator.SetBool("IsDashing", true);
         PlayerBody.gravityScale = 0;
         PlayerBody.velocity = new Vector2(PlayerBody.velocity.x, 0).normalized * 20;
         yield return new WaitForSeconds(0.2f);
         PlayerBody.gravityScale = 1;
         yield return new WaitForSeconds(0.3f);
         IsDashing = false;
+        PlayerAnimator.SetBool("IsDashing", true);
     }
     void Respawn(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -83,11 +93,13 @@ public class PlayerMovement : MonoBehaviour{
             IsGrounded = true;
             HasJumped = false;  
             HasDashed = false;
+            PlayerAnimator.SetBool("IsJumping", false);
         }
     }
     private void OnCollisionExit2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Ground")){
             IsGrounded = false;
+            PlayerAnimator.SetBool("IsJumping", true);
         }
     }
 }
